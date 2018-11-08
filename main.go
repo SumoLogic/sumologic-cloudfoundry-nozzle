45d19a1ecf112efa333399f09955ea6c58e124c1
Duc Ha <duchatran@gmail.com>
2018-11-07 23:02:28 -0800

    Remove Godeps folder, update README.md, fix parsing bug for post minimum delay and update GOPATH in tile.yml

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ var (
 	user                = kingpin.Flag("cloudfoundry_user", "Cloud Foundry User").Envar("CLOUDFOUNDRY_USER").String() //user created in CF, authorized to connect the firehose
 	password            = kingpin.Flag("cloudfoundry_password", "Cloud Foundry Password").Envar("CLOUDFOUNDRY_PASSWORD").String()
 	keepAlive, errDt    = time.ParseDuration("25s") //default Error,ContainerMetric,HttpStart,HttpStop,HttpStartStop,LogMessage,ValueMetric,CounterEvent
+	defaultPostMinimumDelay, _ = time.ParseDuration("2000ms") 
 	wantedEvents        = kingpin.Flag("events", fmt.Sprintf("Comma separated list of events you would like. Valid options are %s", eventRouting.GetListAuthorizedEventEvents())).Default("LogMessage").Envar("EVENTS").String()
 	boltDatabasePath    = "event.db"
 	skipSSLValidation   = kingpin.Flag("skip_ssl_validation", "Skip SSL validation (to allow things like self-signed certs). Do not set to true in production").Default("false").Envar("SKIP_SSL_VALIDATION").Bool()
@@ -96,7 +97,14 @@ func main() {
 		logging.Info.Println("Creating queue for endpoint: " + sumoConfig.Endpoint)
 		queue := eventQueue.NewQueue(make([]*events.Event, 100))
 		queues[i] = &queue
-		loggingClientSumo := sumoCFFirehose.NewSumoLogicAppender(sumoConfig.Endpoint, 5000, &queue, *eventsBatchSize, sumoConfig.PostMinimumDelay, sumoConfig.Category, sumoConfig.Name, sumoConfig.Host, *verboseLogMessages, sumoConfig.CustomMetadata, sumoConfig.IncludeOnlyMatchingFilter, sumoConfig.ExcludeAlwaysMatchingFilter, version)
+
+		postMinDelay, errDPt    := time.ParseDuration(sumoConfig.PostMinimumDelay) 
+		if (errDPt != nil) {
+			logging.Info.Println("Error parsing PostMinimumDelay, got: "+ sumoConfig.PostMinimumDelay+ ". Will be using default value of 2s instead")
+			postMinDelay = defaultPostMinimumDelay
+		}
+		logging.Info.Printf("Using post minimum delay: %v\n",postMinDelay)
+		loggingClientSumo := sumoCFFirehose.NewSumoLogicAppender(sumoConfig.Endpoint, 5000, &queue, *eventsBatchSize, postMinDelay, sumoConfig.Category, sumoConfig.Name, sumoConfig.Host, *verboseLogMessages, sumoConfig.CustomMetadata, sumoConfig.IncludeOnlyMatchingFilter, sumoConfig.ExcludeAlwaysMatchingFilter, version)
 		go loggingClientSumo.Start() //multi
 	}
 
@@ -138,7 +146,7 @@ func main() {
 
 type sumoConfigStruct struct {
 	Endpoint                    string        `json:"endpoint"`
-	PostMinimumDelay            time.Duration `json:"sumo_post_minimum_delay"`
+	PostMinimumDelay            string		  `json:"sumo_post_minimum_delay"`
 	Category                    string        `json:"sumo_category"`
 	Name                        string        `json:"sumo_name"`
 	Host                        string        `json:"sumo_host"`
